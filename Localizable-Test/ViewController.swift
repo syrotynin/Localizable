b949eae9612b1804f41395b1f840876df313d247
Serhii Syrotynin <sesyr@telenor.dk>
2019-06-09 19:21:14 +0300

    Initial Commit

---
+++ Localizable-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localizable-Test
+//
+//  Created by Serhii Syrotynin on 6/9/19.
+//  Copyright © 2019 Serhii Syrotynin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
